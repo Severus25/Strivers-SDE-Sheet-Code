8b5aa37d67de6bf089c79481cc7890948910ca78
Abhijai Rajawat <77570887+Severus25@users.noreply.github.com>
2023-08-28 11:58:08 +0530

    Create 1. Set Matrix Zero.cpp

---
+++ 1. Set Matrix Zero.cpp
@@ -0,0 +1,28 @@
+// Problem Statement: 
+// Given a matrix if an element in the matrix is 0 then you will have to set its entire column and row to 0 and then return the matrix.
+
+// Probem Solution:
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        unordered_set<int> setRows; 
+        unordered_set<int> setColumns; 
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    setRows.insert(i);
+                    setColumns.insert(j);
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(setRows.count(i) > 0 || setColumns.count(j) > 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
