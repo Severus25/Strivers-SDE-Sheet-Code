f38d88a6200a5877204fdcccb9eb72584434c519
Abhijai Rajawat <77570887+Severus25@users.noreply.github.com>
2023-09-01 11:42:03 +0530

    Create 5. Sort Colors - Sort an array of 0's, 1's and 2's.cpp

---
+++ 5. Sort Colors - Sort an array of 0's, 1's and 2's.cpp
@@ -0,0 +1,49 @@
+// Problem Statement:
+// Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.We will use the 
+// integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+// You must solve this problem without using the library's sort function.
+
+// Example 1:
+// Input: nums = [2,0,2,1,1,0]
+// Output: [0,0,1,1,2,2]
+
+// Example 2:
+// Input: nums = [2,0,1]
+// Output: [0,1,2]
+ 
+// Constraints:
+// n == nums.length
+// 1 <= n <= 300
+// nums[i] is either 0, 1, or 2.
+
+//Problem Solution:
+class Solution {
+    public:
+    void sortColors(vector<int>& nums) 
+    {
+        int temp = 0, low = 0, mid = 0, high = nums.size() - 1;
+    
+        while(mid <= high)
+        {
+            if(nums[mid] == 0)
+            {
+                temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1)
+            {
+                mid++;
+            }
+            else if(nums[mid] == 2)
+            {
+                temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                high--;
+            }
+        }
+    }
+};
