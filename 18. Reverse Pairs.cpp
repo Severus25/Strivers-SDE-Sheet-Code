e581be8a39696fcb9e899751755f2052a52b8862
Abhijai Rajawat <77570887+Severus25@users.noreply.github.com>
2023-09-07 11:43:23 +0530

    Create 18. Reverse Pairs.cpp

---
+++ 18. Reverse Pairs.cpp
@@ -0,0 +1,65 @@
+// Problem Statement:
+// Given an integer array nums, return the number of reverse pairs in the array.
+// A reverse pair is a pair (i, j) where:
+// 0 <= i < j < nums.length and
+// nums[i] > 2 * nums[j].
+ 
+// Example 1:
+// Input: nums = [1,3,2,3,1]
+// Output: 2
+// Explanation: The reverse pairs are:
+// (1, 4) --> nums[1] = 3, nums[4] = 1, 3 > 2 * 1
+// (3, 4) --> nums[3] = 3, nums[4] = 1, 3 > 2 * 1
+
+// Example 2:
+// Input: nums = [2,4,3,5,1]
+// Output: 3
+// Explanation: The reverse pairs are:
+// (1, 4) --> nums[1] = 4, nums[4] = 1, 4 > 2 * 1
+// (2, 4) --> nums[2] = 3, nums[4] = 1, 3 > 2 * 1
+// (3, 4) --> nums[3] = 5, nums[4] = 1, 5 > 2 * 1
+
+//Problem Solution:
+class Solution {
+private:
+    int count;
+   
+    void checkCount(vector<int>& nums, int start, int mid, int end){
+
+        // two pointers;
+        int l = start, r = mid + 1;
+        while(l <= mid && r <= end){
+            if((long)nums[l] > (long) 2 * nums[r]){
+                count += (mid - l + 1);
+                r++;
+            }else{
+                l++;
+            }
+        }
+       // worst case might be nlog(n) 
+        sort(nums.begin() + start, nums.begin() + end + 1);
+        return;
+        
+        
+        
+        //every step sort
+    }
+    void mergeSort(vector<int>& nums, int start, int end){
+        if(start == end) return;
+        
+        int mid = (start + end)/2;
+        mergeSort(nums,start, mid);
+        mergeSort(nums,mid+1,end);
+        
+        checkCount(nums,start,mid,end);
+        return;
+        
+    }
+public:
+    int reversePairs(vector<int>& nums) {
+        if(!nums.size())return 0;
+        count = 0;
+        mergeSort(nums,0,nums.size()-1);
+        return count;
+    }
+};
