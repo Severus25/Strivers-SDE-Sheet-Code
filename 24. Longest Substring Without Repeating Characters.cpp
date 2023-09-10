c0658a8c9c073f3bc9e657a2405b92036c830145
Abhijai Rajawat <77570887+Severus25@users.noreply.github.com>
2023-09-10 11:16:14 +0530

    Create 24. Longest Substring Without Repeating Characters.cpp

---
+++ 24. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,35 @@
+// Problem Statement:
+// Given a string s, find the length of the longest 
+// substring without repeating characters.
+
+// Example 1:
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+// Example 2:
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+
+// Example 3:
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+// Problem Solution:
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256, -1);
+        int maxLen = 0, start = -1;
+        for (int i = 0; i != s.length(); i++) {
+            if (dict[s[i]] > start)
+                start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i - start);
+        }
+        return maxLen;
+    }
+};
