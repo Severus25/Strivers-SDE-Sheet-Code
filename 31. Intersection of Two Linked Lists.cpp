0b65db792f84f818f6876445387601dd6ef67550
Abhijai Rajawat <77570887+Severus25@users.noreply.github.com>
2023-09-14 11:57:23 +0530

    Create 31. Intersection of Two Linked Lists.cpp

---
+++ 31. Intersection of Two Linked Lists.cpp
@@ -0,0 +1,47 @@
+// Problem Statement:
+// Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. 
+// If the two linked lists have no intersection at all, return null.
+// For example, the following two linked lists begin to intersect at node c1:
+// The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+// Note that the linked lists must retain their original structure after the function returns.
+
+// Example 1:
+// Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+// Output: Intersected at '8'
+// Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+// From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+// - Note that the intersected node's value is not 1 because the nodes with value 1 in A and B (2nd node in A and 3rd node in B) are different node references. In other words, they point to two different locations in memory, while the nodes with value 8 in A and B (3rd node in A and 4th node in B) point to the same location in memory.
+
+// Example 2:
+// Input: intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+// Output: Intersected at '2'
+// Explanation: The intersected node's value is 2 (note that this must not be 0 if the two lists intersect).
+// From the head of A, it reads as [1,9,1,2,4]. From the head of B, it reads as [3,2,4]. There are 3 nodes before the intersected node in A; There are 1 node before the intersected node in B.
+
+// Example 3:
+// Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+// Output: No intersection
+// Explanation: From the head of A, it reads as [2,6,4]. From the head of B, it reads as [1,5]. Since the two lists do not intersect, intersectVal must be 0, while skipA and skipB can be arbitrary values.
+// Explanation: The two lists do not intersect, so return null.
+// Problem Solution:
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
+    {
+        ListNode *p1 = headA;
+        ListNode *p2 = headB;
+            
+        if (p1 == NULL || p2 == NULL) return NULL;
+
+        while (p1 != NULL && p2 != NULL && p1 != p2) {
+            p1 = p1->next;
+            p2 = p2->next;
+
+            if (p1 == p2) return p1;
+
+            if (p1 == NULL) p1 = headB;
+            if (p2 == NULL) p2 = headA;
+        }
+        return p1;
+    }
+};
