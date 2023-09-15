954b954e3abe17c13e577aa9b40dbc34e6c44d5e
Abhijai Rajawat <77570887+Severus25@users.noreply.github.com>
2023-09-15 12:36:27 +0530

    Create 34. Palindrome Linked List.cpp

---
+++ 34. Palindrome Linked List.cpp
@@ -0,0 +1,29 @@
+// Problem Statement:
+// Given the head of a singly linked list, return true if it is a 
+// palindrome or false otherwise.
+
+// Example 1:
+// Input: head = [1,2,2,1]
+// Output: true
+
+// Example 2:
+// Input: head = [1,2]
+// Output: false
+  
+// Problem Solution:
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode *slow = head, *fast = head, *prev, *temp;
+        while (fast && fast->next)
+            slow = slow->next, fast = fast->next->next;
+        prev = slow, slow = slow->next, prev->next = NULL;
+        while (slow)
+            temp = slow->next, slow->next = prev, prev = slow, slow = temp;
+        fast = head, slow = prev;
+        while (slow)
+            if (fast->val != slow->val) return false;
+            else fast = fast->next, slow = slow->next;
+        return true;
+    }
+};
