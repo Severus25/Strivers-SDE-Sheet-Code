4ffa20230185f73cd0f82743c1c60a73e8fa1e27
Abhijai Rajawat <77570887+Severus25@users.noreply.github.com>
2023-09-18 10:57:46 +0530

    Create 39. 3Sum.cpp

---
+++ 39. 3Sum.cpp
@@ -0,0 +1,65 @@
+// Problem Statement:
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that 
+// i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+// Notice that the solution set must not contain duplicate triplets.
+
+// Example 1:
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+// Explanation: 
+// nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+// nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+// nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+// The distinct triplets are [-1,0,1] and [-1,-1,2].
+// Notice that the order of the output and the order of the triplets does not matter.
+
+// Example 2:
+// Input: nums = [0,1,1]
+// Output: []
+// Explanation: The only possible triplet does not sum up to 0.
+
+// Example 3:
+// Input: nums = [0,0,0]
+// Output: [[0,0,0]]
+// Explanation: The only possible triplet sums up to 0.
+
+// Problem Solution:
+class Solution {
+public:
+    vector<vector<int> > threeSum(vector<int> &num) {
+        vector<vector<int> > res;
+        sort(num.begin(), num.end());
+        
+        for (int i = 0; i < num.size(); i++) {
+            int target = -num[i];
+            int front = i + 1;
+            int back = num.size() - 1;
+            while (front < back) {
+                int sum = num[front] + num[back];
+                // Finding answer which start from number num[i]
+                if (sum < target)
+                    front++;
+
+                else if (sum > target)
+                    back--;
+
+                else {
+                    vector<int> triplet = {num[i], num[front], num[back]};
+                    res.push_back(triplet);
+                    
+                    // Processing duplicates of Number 2
+                    // Rolling the front pointer to the next different number forwards
+                    while (front < back && num[front] == triplet[1]) front++;
+
+                    // Processing duplicates of Number 3
+                    // Rolling the back pointer to the next different number backwards
+                    while (front < back && num[back] == triplet[2]) back--;
+                } 
+            }
+            // Processing duplicates of Number 1
+            while (i + 1 < num.size() && num[i + 1] == num[i]) 
+                i++;
+        }
+        return res;
+    }
+};
