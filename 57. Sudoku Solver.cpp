1e7ce7c273af8edd1c3d87350d20706c42d1cf9f
Abhijai Rajawat <77570887+Severus25@users.noreply.github.com>
2023-09-27 18:54:49 +0530

    Create 57. Sudoku Solver.cpp

---
+++ 57. Sudoku Solver.cpp
@@ -0,0 +1,57 @@
+// Problem Statement:
+// Write a program to solve a Sudoku puzzle by filling the empty cells.
+// A sudoku solution must satisfy all of the following rules:
+// Each of the digits 1-9 must occur exactly once in each row.
+// Each of the digits 1-9 must occur exactly once in each column.
+// Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+// The '.' character indicates empty cells.
+
+// Example 1:
+// Input: board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],
+// ["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],
+// [".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+// Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],
+// ["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],
+// ["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
+
+// Problem Solution:
+class Solution {
+private:
+    bool isValid(vector<vector<char>>& board, int row, int col, char ch){
+        for(int i=0; i<9; i++){
+            if(board[i][col] == ch){
+                return false;
+            }
+            if(board[row][i] == ch){
+                return false;
+            }
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3] == ch){
+                return false;
+            }
+        }
+        return true;
+    }
+    bool solve(vector<vector<char>>& board) {
+        for(int i=0; i<board.size(); i++){
+            for(int j=0; j<board[0].size(); j++){
+                if(board[i][j] == '.'){
+                    for(char ch='1'; ch<='9'; ch++){
+                        if(isValid(board, i, j, ch)){
+                            board[i][j] = ch;
+                            if(solve(board) == true){
+                                return true;
+                            }
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};
