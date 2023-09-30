ff1a4bd8d823e254b398d5f599f581b0a78323c2
Abhijai Rajawat <77570887+Severus25@users.noreply.github.com>
2023-09-30 10:49:33 +0530

    Create 64. Search in Rotated Sorted Array.cpp

---
+++ 64. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,43 @@
+// Problem Statement:
+// There is an integer array nums sorted in ascending order (with distinct values).
+// Prior to being passed to your function, nums is possibly rotated at an unknown pivot 
+// index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], 
+// nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+// Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+// Output: 4
+
+// Example 2:
+// Input: nums = [4,5,6,7,0,1,2], target = 3
+// Output: -1
+
+// Example 3:
+// Input: nums = [1], target = 0
+// Output: -1
+
+// Problem Solution:
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int s = 0, e = n-1;
+        while(s<=e){
+            int m=s+(e-s)/2;
+            if(nums[m] == target)
+                return m;
+            if(target >= nums[0] && nums[m] < nums[0])
+                e = m - 1;
+            else if(target < nums[0] && nums[m] >= nums[0])
+                s=m+1;
+            else
+                if(nums[m]<target)
+                    s=m+1;
+                else 
+                    e=m-1;
+        }
+        return -1;
+    }
+};
